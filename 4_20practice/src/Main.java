107479158072b521940c88006b3a0a71f1a65f07
YuMomo <_github_@example.com>
2026-04-30 00:21:44 +0800

    表达式求值异常处理

--- 4_20practice/src/Main.java
+++ 4_20practice/src/Main.java
@@ -1,8 +1,41 @@
 import java.util.*;
-
+//static关键字意味着此变量属于类本身，而不属于main方法，相当于在Main类里大家共享
 public class Main {
+    static HashMap <String,variable>  va = new HashMap();
     static String expr;  //整个表达式字符串
     static int pos; //当前扫描到的位置
+
+    //定义一个变量容器
+    static class variable{
+        String type;//int or float
+        Number value;
+        boolean initialized;//是否赋过初值
+        public  variable(String type){
+            this.type=type;
+            this.value=null;
+            this.initialized=false;
+
+        }
+        public variable(String type, Number value) throws Exception {
+           this.type=type;
+           this.value=value;
+           if(type.equals("int") && value instanceof Integer){
+              
+           } else if (type.equals("float") && value instanceof Float) {
+              
+           }else if (type.equals("float") && value instanceof Integer) {
+              float v=value.floatValue();
+              Float fv=v;
+              this.value=fv;
+           }else if (type.equals("int") && value instanceof Float){
+               int v=value.intValue();
+               Integer iv=v;
+               this.value=iv;
+           }
+           else throw new Exception("wrong - error expresion");//类型和变量不匹配
+           initialized=true;//已初始化===========================
+      }
+    }
     //跳过空格
     static void skipSpaces(){
         while(pos<expr.length()&&expr.charAt(pos)==' '){
@@ -10,7 +43,18 @@ public class Main {
         }
     }
 
+    //把source转换成数字
+    /*public static Number toNumber(String s){
+        try{
+            return Integer.parseInt(s);
+        }catch (Exception e){
+            try{
+                return Float.parseFloat(s);
 
+            }catch(Exception o){}
+            throw  new IllegalArgumentException("不是合法数字");
+        }
+    }*/
 
     //读取一个数字
     static Number parseNumber(){
@@ -30,6 +74,15 @@ public class Main {
         }else{
             return Integer.parseInt(Str);
         }
+    }
+
+    //新增：用于根据变量名从hashMap中取值
+    static Number getVariableVaulue(String varName)throws Exception{
+        if(!va.containsKey(varName)) throw new Exception("wrong - variable undefined");
+        variable v=va.get(varName);
+        if(v==null) throw new Exception("wrong - variable undefined");
+        if(!v.initialized) throw new Exception("wrong - variable unassigned");
+        return v.value;
     }
 
     //执行运算并且做类型提升
@@ -74,28 +127,111 @@ public class Main {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         //while(sc.hasNextLine()){
-            String line =  sc.nextLine().trim();
-            //if(line.isEmpty()) break;
-            if(line.endsWith("=")){
-                line=line.substring(0,line.length()-1);
+        while(sc.hasNext()){
+        
+            try{
+                String stat=sc.nextLine().replaceAll("[;]$","").trim();
+                if(stat.endsWith("?")) {
+                    String stat1 = stat.replaceAll("[=?]+$","");
+                    expr=stat1; 
+                    pos=0;
+                    Number result = parseExpr();
+                    skipSpaces();
+                    if(pos<expr.length()) throw new Exception("wrong - error expression");
+                    if(result instanceof Integer){
+                        System.out.println(result);
+                    }
+                    else{
+                        System.out.printf("%.2f\n",result.floatValue());
+                    }
+                }
+                else if(stat.contains("=")){
+                    //赋值语句
+                    String[]stats =stat.split("=");
+                    String target = stats[0];
+                    String source = stats[1].trim();
+                    String typestr="";
+                    String valuename="";
 
+                    if(target.trim().contains(" ")){//声明并初始化的语句
+                        typestr=target.trim().split(" ")[0];
+                        valuename=target.trim().split(" ")[1];
+                        if(va.containsKey(valuename)){
+                            //重复声明
+                            throw new Exception("Redifinition");
+                        }else{
+                            
+                            //计算source的值
+                            expr=source;
+                            pos=0;
+                            Number ss=parseExpr();
+                            variable zhi=new variable(typestr,ss);
+                            va.put(valuename,zhi);
+                        }
+                    }
+                    else{
+                        //不带类型的赋值语句
+                        valuename=target.trim();
+
+                        //计算source的值
+                        expr=source;
+                        pos=0;
+                        Number ss=parseExpr();
+
+                        variable v=va.get(valuename);
+                        if(v==null) throw new Exception("wrong - variable undefined");
+                        if(v.type.equals("int")){
+                            v.value=ss.intValue();
+                        } else if(v.type.equals("float")){
+                            v.value=ss.floatValue();
+                        } else{
+                            throw new Exception("wrong - variable type");
+                        }       
+                        v.initialized=true;       
+                    }
+
+                    
+
+                }
+                else{
+                    //声明语句 
+                    String typestr="";
+                    String valuename="";
+                    //变量声明语句
+                    
+                    //Number fuzhi=getVariableVaulue(valuename); //检查变量类型，没有就报错
+
+                    if(stat.trim().contains(" ")){
+                        typestr=stat.trim().split(" ")[0];
+                        valuename=stat.trim().split(" ")[1];
+                        if(va.containsKey(valuename)){
+                            //重复声明
+                            throw new Exception("Redifinition");
+                        }else{
+                            variable zhi=new variable(typestr);
+                            va.put(valuename,zhi);
+                        }
+
+                    }
+                    else
+                        throw new Exception("wrong - declaration");
+                }
+            }catch(NoSuchMethodError e){
+                System.out.println("wrong - Constructor" );
+            
+            }catch(ClassNotFoundException e){
+                System.out.println("Wrong Type");
+            }catch(Exception e){
+                System.out.println(e.getMessage() );
             }
-            expr=line;
-            pos=0; //每处理一行就要重置
-            Number result=parseExpr();
-            //用Number来统一Integer Float
+        }
+           
 
-            if(result instanceof Float){
-                System.out.println(result);
-            }else{
-                System.out.println(result);
-            }
-        //}
 
-    }
+}
 
     //解析加减（最低优先级）
-    static Number parseExpr(){
+    static Number parseExpr() throws Exception {
         Number left = parseTerm();
         while(true){
             skipSpaces();//关键:在查看op之间跳过空格
@@ -110,11 +246,12 @@ public class Main {
             }
         }
         return left;
+
     }
 
 
     //解析乘除余（中优先级）
-    static Number parseTerm(){
+    static Number parseTerm() throws Exception {
 
         Number left = parseFactor();
         while(true){
@@ -135,23 +272,38 @@ public class Main {
     }
 
     //解析括号和数字（高优先级）
-    static Number parseFactor(){
+    static Number parseFactor()throws Exception{
 
             skipSpaces();
-
+        char c=expr.charAt(pos);
         //处理一元负号，例如（-5+3）
-        if(expr.charAt(pos)=='-'){
+        if(c=='-'){
             pos++;
             return applyOp('*',-1,parseFactor());
         }
 
-        if(pos<expr.length() && expr.charAt(pos) == '('){
+        if(pos<expr.length() && c == '('){
             pos++;  //跳过‘（’
             //括号内重新从最低级优先级开始算
             Number val = parseExpr();
-            pos++; //跳过“）”
+            //检查是否有括号
+            if(pos < expr.length() && expr.charAt(pos)== ')'){
+                pos++;
+            } else{
+                throw new Exception("wrong - error expression");
+            }
+            
             return val;
+        }else if(Character.isDigit(c) || c=='.' ) {
+            return parseNumber();
+        }else if(Character.isLetter(c)){
+            int start = pos;
+            while(pos<expr.length() && Character.isLetterOrDigit(expr.charAt(pos)))pos++;
+            String varname=expr.substring(start,pos);
+            return getVariableVaulue(varname);
         }
-        return parseNumber();
+
+        throw new Exception("wrong - error expression");
     }
+
 }
